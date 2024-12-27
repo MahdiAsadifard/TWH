137d70621ddb8427eb71b7149186f49cfea6bab9
mahdiasadifard@gmail.com <mahdiasadifard@gmail.com>
2024-12-27 09:13:10 -0500

    Camel-case

--- Webservice/TWHapi/ProgramHelpers/Extensions/OptionsBuilderExtensions.cs
+++ Webservice/TWHapi/ProgramHelpers/Extensions/OptionsBuilderExtensions.cs
@@ -4,11 +4,12 @@
     {
         public static IServiceCollection InitializeOptions(this IServiceCollection services)
         {
-
-            services.AddControllers().AddJsonOptions(options =>
-            {
-                options.JsonSerializerOptions.PropertyNamingPolicy = null;
-            });
+            /// This part will change the format to Pascal-Case
+            /// We are going to use Camel-Case as default behavior
+            //services.AddControllers().AddJsonOptions(options =>
+            //{
+            //    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            //});
             return services;
         }
     }
